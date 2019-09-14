ce1cb45748ddbf43ee9963ceb05257b646d76805
ujstha <ujstha27@gmail.com>
2019-09-14 22:33:05 +0300

    search query can be set in url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Search from './components/Search';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Search />        
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <div style={{ marginTop: '60px', marginBottom: '60px' }}>
+            <Switch>
+              <Route path="/" component={Search} exact></Route>
+              <Route path={`/page/:currentPage`} component={Search}></Route>
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { TextField } from '@material-ui/core';
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const BASE_URL = 'http://www.omdbapi.com/';
 const API_KEY = 'ceb40971';
@@ -13,30 +13,63 @@ export default class Search extends Component {
             movieData: [],
             isLoaded: '',
             query: '',
-            pageLimit: 5
+            pageLimit: 5,
+            paging: null,
         }
+    }
+    componentDidMount(e) {
+        const currentPage = Number(this.props.match.params.currentPage) || Number(e.target.id);
+        const query = new URLSearchParams(this.props.location.search);
+        const searchParam = query.get('s');
+        this.setState({ query: searchParam });
+        if(searchParam === "") {
+            return (
+                this.props.history.push(``)
+            )
+        } else {
+            axios.get(`${BASE_URL}?s=${searchParam}&page=${currentPage}&type=&apikey=${API_KEY}`)
+            .then(res => {
+                if(currentPage === 1) {
+                    this.props.history.push(`?s=${this.state.query}`);
+                } else if (currentPage > 1) {
+                    this.props.history.push(`/page/${currentPage}?s=${this.state.query}`);
+                }
+                console.log(res.data, 'data');
+                const movies = res.data;
+                this.setState({ isLoaded: movies.Response /*return True or False*/, movieData: movies, paging: currentPage });
+                console.log(this.state.paging, 'pgai')
+            })
+        }
+    }
+    
+    UNSAFE_componentWillUpdate() {
+        window.scrollTo(0,0);
     }
 
     onChange = e => {
         this.setState({ [e.target.name]: e.target.value });
         if (e.target.value === '') {
-            this.setState({ movieData: [], isLoaded: '' })
+            this.setState({ movieData: [], isLoaded: '' });
+            this.props.history.push(``);
         }
     };
 
     search = (e) => {
         e.preventDefault();
-        this.state.currentPage = e.target.id || 1;
+        const currentPage = Number(e.target.id) || 1;
+        this.props.history.push(`?s=${this.state.query}`);
         
-        axios.get(`${BASE_URL}?s=${this.state.query}&page=${this.state.currentPage}&type=&apikey=${API_KEY}`)
+        axios.get(`${BASE_URL}?s=${this.state.query}&page=${currentPage}&type=&apikey=${API_KEY}`)
         .then(res => {
+            if(currentPage === 1) {
+                this.props.history.push(`?s=${this.state.query}`);
+            } else if (currentPage > 1) {
+                this.props.history.push(`/page/${currentPage}?s=${this.state.query}`);
+            }
             console.log(res.data, 'data');
             const movies = res.data;
-            this.setState({ isLoaded: movies.Response /*return True or False*/, movieData: movies });
-            console.log(this.state.movieData);
-            console.log(this.state.isLoaded, 'loaded');
+            this.setState({ isLoaded: movies.Response /*return True or False*/, movieData: movies, paging: currentPage });
         })
-        console.log(this.state.query);
     }
 
     searchResult = () => {
@@ -46,16 +79,17 @@ export default class Search extends Component {
                 pageNumbers.push(i);
             }
             const renderPageNumbers = pageNumbers.map(number => {
-                let classes = this.state.currentPage === number ? 'active' : '';
+                let classes = this.state.paging === number ? 'active' : '';
                 return (
-                    <li
-                        key={number}
-                        id={number}
-                        onClick={this.search}
-                        className={classes}
-                    >
-                        {number}
-                    </li>
+                    <Link to={`/page/${number}`} key={number} className={classes}>
+                        <li 
+                            id={number}
+                            onClick={this.search}
+                            className={classes}
+                        >
+                            {number}
+                        </li>
+                    </Link>
                 );
             });
             return (
@@ -70,16 +104,16 @@ export default class Search extends Component {
                         ))}
                     </div>
                     {(renderPageNumbers.length <= 1) ? '' : 
-                        <div className="row">
-                            <ul className="col-md-12 text-center">
-                            {/*eslint-disable-next-line react/no-direct-mutation-state*/}
-                            Page {this.state.currentPage} of {renderPageNumbers.length}
-                            </ul>
-                            <ul id="horizontal-list" className="col-md-12 text-center">
-                            
-                                {renderPageNumbers}
-                            
-                            </ul>
+                        <div className="row page-number">
+                            <div className="col-md-12 text-center">
+                                Page {this.state.paging} of {renderPageNumbers.length}
+                            </div>
+                            <div className="col-md-12">
+                                <ul id="horizontal-list" className="text-center">
+                                    {renderPageNumbers}     
+                                </ul>
+                            </div>
+                            <br />
                         </div>
                     }
                 </div>
@@ -105,7 +139,6 @@ export default class Search extends Component {
                 </form>
                 {this.state.isLoaded === "False" ? <h1>Movie Not Found!</h1> : ''}
                 {this.searchResult()}
-                {console.log(this.state.query)}
             </div>
         )
     }
